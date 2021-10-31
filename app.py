31207b98d37d3b8df4c678f80a357a61f64600f7
aihulks69 <93446507+aihulks69@users.noreply.github.com>
2021-11-01 05:05:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Create a directory in a known location to save files to.
 uploads_dir = os.path.join(os.getcwd(), "instance/uploads")
 
-@app.route('/audio', methods=['POST'])
+@app.route('/', methods=['POST'])
 def get_score():
     if request.method == 'POST':
       f = request.files['file']
