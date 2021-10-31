9794124617fdcdad18ba5da5e634a20ea56c975b
aihulks69 <93446507+aihulks69@users.noreply.github.com>
2021-11-01 05:18:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 # Create a directory in a known location to save files to.
 uploads_dir = os.path.join(os.getcwd(), "instance/uploads")
 
-@app.route('/', methods=['POST'])
+@app.route('/audio', methods=['POST'])
 def get_score():
     if request.method == 'POST':
       f = request.files['file']
       f.save(os.path.join(uploads_dir, f.filename))
       denoise_signal()
       return 'file uploaded successfully'
+    else:
+        return 'ERROR'
 
 
 def denoise_signal():
